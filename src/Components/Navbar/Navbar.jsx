41658f166c740b4739c3c3fde8f01b44faa1d8e9
suganyarajes <sugansrib@gmail.com>
2024-04-25 10:45:51 -0500

    main

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,97 +3,60 @@ import { useNavigate, NavLink } from "react-router-dom";
 export default function Navbar() {
   const navigator = useNavigate();
   return (
-    <nav className="navbar navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <div className="row" style={{ width: "100%" }}>
-          <div className="col-6">
-            <a className="navbar-brand" href="#">
-              Sq<span style={{color:'red'}}>U</span>are
-            </a>
+        <a className="navbar-brand" href="#">
+          Sq<span style={{ color: "red" }}>U</span>are
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="navbar-nav">
+            <NavLink
+              exact
+              to="/all"
+              className="nav-link"
+              activeClassName="active"
+            >
+              ALL
+            </NavLink>
+            <NavLink to="/fsd" className="nav-link" activeClassName="active">
+              FULLSTACK
+            </NavLink>
+            <NavLink to="/db" className="nav-link" activeClassName="active">
+              DATABASE
+            </NavLink>
+            <NavLink to="/fe" className="nav-link" activeClassName="active">
+              FRONTEND
+            </NavLink>
+            <NavLink to="/be" className="nav-link" activeClassName="active">
+              BACKEND
+            </NavLink>
           </div>
-          <div className="col-6">
-            <div className="row">
-              <div className="col-8">
-                <ul className="nav">
-                  <li className="nav-item">
-                    <NavLink
-                      to="/all"
-                      className={({ isActive, isPending }) =>
-                        isPending
-                          ? "pending"
-                          : isActive
-                          ? "nav-link active"
-                          : "nav-link"
-                      }
-                      aria-current="page"
-                    >
-                      ALL
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink
-                      to="/fsd"
-                      className={({ isActive, isPending }) =>
-                        isActive ? "nav-link active" : "nav-link"
-                      }
-                      aria-current="page"
-                    >
-                      FULLSTACK
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink
-                      to="/db"
-                      className={({ isActive, isPending }) =>
-                        isActive ? "nav-link active" : "nav-link"
-                      }
-                      aria-current="page"
-                    >
-                     DATABASE
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink
-                      to="/fe"
-                      className={({ isActive, isPending }) =>
-                        isActive ? "nav-link active" : "nav-link"
-                      }
-                      aria-current="page"
-                    >
-                     FRONTEND
-                    </NavLink>
-</li>
-
-                  <li className="nav-item">
-                    <NavLink
-                      to="/be"
-                      className={({ isActive, isPending }) =>
-                        isActive ? "nav-link active" : "nav-link"
-                      }
-                      aria-current="page"
-                    >
-                    BACKEND
-                    </NavLink>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-4 d-grid gap-2 d-md-block">
-                <button
-                  type="button"
-                  className="btn btn-outline-primary mx-2"
-                  onClick={() => navigator("/login")}
-                >
-                  Login
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-primary"
-                  onClick={() => navigator("/signup")}
-                >
-                  Signup
-                </button>
-              </div>
-            </div>
+          <div className="ms-lg-auto">
+            <button
+              type="button"
+              className="btn btn-outline-primary me-2"
+              onClick={() => navigator("/login")}
+            >
+              Login
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={() => navigator("/signup")}
+            >
+              Signup
+            </button>
           </div>
         </div>
       </div>
